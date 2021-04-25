d7f646f21516850a178fd231232fd2cddf229cf3
Sara Amer <saraamersaad8@gmail.com>
2021-04-25 20:34:17 +0200

    Display data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+class App extends React.Component {
 
-function App() {
+  render()
+  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+   <Data/>
     </div>
   );
+  }
+}
+class Data extends React.Component{
+  constructor()
+  {
+    super();
+  this.data={
+     "id":"1",
+     "jobTitleName":"Developer",
+     "firstName":"Ahmed",
+     "lastName":"Ali",
+     "preferredFullName":" Ahmed Ali ",
+     "employeeCode":"E1",
+     "region":"CA",
+     "phoneNumber":"408-1234567",
+     "emailAddress":" ahmed.ali @gmail.com"
+     }
+   }
+  render()
+  {
+    return (
+      <div>
+        <table>
+  <tr>
+    <th>ID</th>
+    <th> jobTitleName </th>
+    <th>firstName</th>
+    <th>lastName</th>
+    <th>preferredFullName</th>
+    <th>employeeCode</th>
+    <th>region</th>
+    <th>phoneNumbe</th>
+    <th>emailAddress</th>
+  </tr>
+  <tr>
+    <td>{this.data.id}</td>
+    <td>{this.data.jobTitleName}</td>
+    <td>{this.data.firstName}</td>
+    <td>{this.data.lastName}</td>
+    <td>{this.data.preferredFullName}</td>
+    <td>{this.data.employeeCode}</td>
+    <td>{this.data.region}</td>
+    <td>{this.data.phoneNumbe}</td>
+    <td>{this.data.emailAddress}</td>
+    
+    
+  </tr>
+  <tr></tr>
+
+
+</table>
+      </div>
+
+
+    )
+  } 
+  
 }
 
 export default App;
